39cf2269bb2592fc14da62ac7298515e7f8e94f9
Von Delos Santos <vdelossantos@you-source.com>
2021-10-09 18:50:24 +0800

    Initial Commit

---
+++ TodoApp.Api/Startup.cs
@@ -0,0 +1,53 @@
+namespace TodoApp.Api
+{
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.Extensions.DependencyInjection;
+
+    public class Startup
+    {
+        #region ConfigureServices
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+        #endregion
+
+        #region Configure
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Hello World!");
+                });
+            });
+        }
+
+        public void ConfigureLocal(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseDeveloperExceptionPage();
+            this.Configure(app, env);
+        }
+
+        public void ConfigureDevelopment(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            app.UseDeveloperExceptionPage();
+            this.Configure(app, env);
+        }
+
+        public void ConfigureStaging(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            this.Configure(app, env);
+        }
+
+        public void ConfigureProduction(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            this.Configure(app, env);
+        }
+        #endregion
+    }
+}

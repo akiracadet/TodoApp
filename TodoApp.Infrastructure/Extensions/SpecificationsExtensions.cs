abd9c6bdec6e014ae7eb57ec07cbdec10aa6316d
Von Delos Santos <vdelossantos@you-source.com>
2021-10-11 19:40:18 +0800

    updated specifications

--- TodoApp.Infrastructure/Extensions/SpecificationsExtensions.cs
+++ TodoApp.Infrastructure/Extensions/SpecificationsExtensions.cs
@@ -2,17 +2,17 @@
 {
     using System.Collections.Generic;
     using System.Linq;
-    using TodoApp.Infrastructure.Specifications;
+    using TodoApp.Infrastructure.Interfaces;
 
     public static class SpecificationsExtensions
     {
-        public static bool IsSatisfied<T>(this List<Specification<T>> specifications)
+        public static bool IsSatisfied<T>(this List<ISpecification<T>> specifications)
         {
             var result = specifications.All(spec => spec.IsSatisfied());
             return result;
         }
 
-        public static IEnumerable<string> Errors<T>(this List<Specification<T>> specifications)
+        public static IEnumerable<string> Errors<T>(this List<ISpecification<T>> specifications)
         {
             var result = specifications.Where(spec => !spec.IsSatisfied()).Select(spec => spec.Error);
             return result;
